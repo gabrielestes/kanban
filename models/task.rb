e3b20118588ac96ac7d25f594116b0814e35d070
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-10 14:36:28 -0400

    Changed class comment

--- models/task.rb
+++ models/task.rb
@@ -1,7 +1,7 @@
 require 'active_record'
 require_relative '../db/migrate/001_create_tasks.rb'
 
-# movie model
+# task model
 class Task < ActiveRecord::Base
   validates :id, :title, :status, :priority, presence: true
 end
